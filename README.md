ba7c975ee60eec8e0d257376c854b44100b7780b
Jakob Grønhaug <j@kob.codes>
2020-07-24 10:44:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Terminal time
+It's terminal time!
+
+For when you don't know what time it is and you're near-sighted like me.
+As a bonus, this script does not use ~30% of my CPU while idling like some of the existing solutions I was able to find on pip do.
+
+Only tested with Python 3.7, but should work in Python <= 3.5 since that's when type hints were added.
+I personally use this by adding it as a bash alias:
+
+    alias clock="py ~/tools/terminal_time/clock.py"
+
+## Usage
+
+    usage: clock.py [-h] [-l] [-s] [-f FILENAME]
+
+    What time is it? I'm near-sighted.
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    -l, --loop            The clock "takes over" the terminal it's in, instead
+                            of printing the current time and exiting
+    -s, --seconds         Show seconds. By default time is shown as HH:MM,
+                            adding this flag changes format to HH:MM:SS
+    -f FILENAME, --filename FILENAME
+                            File to load the font from. Default uses the bundled
+                            font file
+
+The argument -l/--loop can be passed to have the clock clear the terminal it's called in and stay there, refreshing every few seconds.
+Shrink your terminal and pin it with your window manager of choice to always have this nice, big clock visible without squinting, because vision tests and glasses are expensive but this little script is free.
+
+The argument -s/--seconds can be passed to show HH:MM:SS instead of the default HH:MM.
+Using both -l and -s at the same time is not recommended as there is a noticeable flicker or blink whenever the clock updates, and when -ls is passed this happens once per second.
+
+## Customizability
+Make a copy of the *font* file, and edit away!
+Open the new *font* file in your text editor of choice, and you can change the symbols that are used to display the time.
+The *font* file contains the digits 0-9 and a separator, which is : by default.
+All symbols in *font* should be of equal height, and all lines within a symbol should be of the same length.
+
+When you're happy with your font, pass -f <*your_font_file*> when running clock.py to use it!
---
+++ clock.py
@@ -0,0 +1,67 @@
+from typing import TypeVar
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
+import re, os, threading
+
+parser = ArgumentParser(description='What time is it? I\'m near-sighted.')
+parser.add_argument('-l', '--loop',
+                    help='The clock "takes over" the terminal it\'s in, instead of printing the current time and exiting', 
+                    action='store_true')
+
+parser.add_argument('-s', '--seconds',
+                    help='Show seconds. By default time is shown as HH:MM, adding this flag changes format to HH:MM:SS',
+                    action='store_true')
+
+parser.add_argument('-f', '--filename',
+                    help='File to load the font from. Default uses the bundled font file')
+
+Number = TypeVar('Number', int, float, complex)
+
+
+def get_digit(number: Number, n: Number) -> int:
+    return number // 10**n % 10
+
+
+def clock(loop: bool, showSeconds: bool) -> None:
+    currentTime = datetime.now().time()
+    hours = currentTime.hour
+    minutes = currentTime.minute
+    seconds = currentTime.second
+
+    global prevTime
+
+    if showSeconds or minutes != prevTime.minute:
+        prevTime = currentTime
+
+        printbuf = ''
+        if loop:
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        for offset in range(fontSize):
+            printbuf += fontbuf[fontSize * get_digit(hours, 1) + offset] + fontbuf[fontSize * get_digit(hours, 0) + offset]
+            printbuf += fontbuf[10 * fontSize + offset]
+            printbuf += fontbuf[fontSize * get_digit(minutes, 1) + offset] + fontbuf[fontSize * get_digit(minutes, 0) + offset]
+            if showSeconds:
+                printbuf += fontbuf[10 * fontSize + offset]
+                printbuf += fontbuf[fontSize * get_digit(seconds, 1) + offset] + fontbuf[fontSize * get_digit(seconds, 0) + offset]
+            printbuf += '\n'
+
+        print(printbuf, end='')
+    if loop:
+        threading.Timer(1 if showSeconds else 10, clock, args=(loop, showSeconds)).start()
+
+
+args = parser.parse_args()
+
+fontFile = args.filename if args.filename else 'font'
+
+printbuf = ''
+
+prevTime = datetime.today() - timedelta(minutes=1)
+
+with open(fontFile, 'r') as f:
+    fontbuf = re.split('\n', f.read())
+
+fontSize = len(fontbuf) // 11
+
+clock(args.loop, args.seconds)
